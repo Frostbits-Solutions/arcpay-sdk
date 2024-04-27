14b4a4e666aec35d348cf00ef0c77d6429d7446f
n0lk <89748313+nzerolk@users.noreply.github.com>
2024-04-27 15:01:10 -0400

    Tailwind set-up

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'selector',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'slide-in-bottom': 'slide-in-bottom .5s var(--ease-spring-2) forwards',
+        'modal': 'slide-in-bottom .5s var(--ease-spring-2) both .2s',
+        'blur': 'blur .2s linear forwards',
+      },
+      keyframes: {
+        'slide-in-bottom': {
+          '0%': { transform: 'translateY(10vh)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
+        'blur': {
+          '0%': { backdropFilter: 'blur(0)', opacity: 0 },
+          '100%': { backdropFilter: 'blur(4px)', opacity: 1 },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
