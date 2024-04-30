3f01b4c7aae1771033fec6d9d18f827f20db749d
AChaffangeon <chaffangeon.adrien@gmail.com>
2024-04-30 20:09:26 +0200

    Update BuyButton.vue

--- src/components/Transaction/BuyButton.vue
+++ src/components/Transaction/BuyButton.vue
@@ -1,4 +1,5 @@
 <template>
+  <IntInput v-model="parameterStore.price" v-if="transactionStore.contractType === CONTRACT_TYPE.Dutch"/>
   <button
     class="ap-flex ap-items-center ap-justify-center ap-rounded-lg ap-bg-blue-700 ap-px-5 ap-py-2.5 ap-text-center ap-text-sm ap-font-medium ap-text-white hover:ap-bg-blue-800 focus:ap-outline-none focus:ap-ring-4 focus:ap-ring-blue-300 dark:ap-bg-blue-600 dark:ap-hover:bg-blue-700 dark:focus:ap-ring-blue-800"
     @click="transactionStore.doTransaction()">Buy</button>
@@ -6,8 +7,12 @@
 
 <script setup>
 import {useTransactionStore} from '@/stores/transactionStore'
+import {CONTRACT_TYPE} from '@/constants/index.js'
+import {useParametersStore} from '@/stores/parametersStore.js'
+import IntInput from '@/components/IntInput.vue'
 
 const transactionStore = useTransactionStore()
+const parameterStore = useParametersStore()
 </script>
 
 <style scoped>
