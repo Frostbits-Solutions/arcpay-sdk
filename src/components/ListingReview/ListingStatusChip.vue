7bb9fdf31e96b755920416a5371cb984796d0bb7
n0lk <89748313+nzerolk@users.noreply.github.com>
2024-09-14 19:13:02 -0400

    Mobile friendly styles

--- src/components/ListingReview/ListingStatusChip.vue
+++ src/components/ListingReview/ListingStatusChip.vue
@@ -30,11 +30,11 @@ defineExpose({
           {{ status }}
         </span>
     <span v-if="['active'].includes(status)"
-          class="ap-inline-flex ap-items-center ap-bg-purple-100/60 ap-text-purple-800 ap-text-xs ap-font-medium ap-px-2.5 ap-py-1.5 ap-rounded-full dark:ap-bg-purple-900 dark:ap-text-purple-300">
+          class="ap-inline-flex ap-items-center ap-bg-purple-100/60 ap-text-purple-800 ap-text-xs ap-font-medium ap-px-2.5 ap-py-1.5 ap-rounded-full dark:ap-bg-purple-900 dark:ap-text-purple-300 ap-relative">
       <span class="ap-w-2 ap-h-2 ap-me-1 ap-bg-purple-500 ap-rounded-full ap-animate-ping"></span>
       <span class="ap-w-1.5 ap-h-1.5 ap-ml-[1px] ap-bg-purple-500 ap-rounded-full ap-absolute"></span>
           {{ status }}
-        </span>
+    </span>
     <span v-if="['closed', 'ended'].includes(status)"
           class="ap-inline-flex ap-items-center ap-bg-green-100 ap-text-green-800 ap-text-xs ap-font-medium ap-px-2.5 ap-py-1.5 ap-rounded-full dark:ap-bg-green-900 dark:ap-text-green-300">
           <span class="ap-w-2 ap-h-2 ap-me-1 ap-bg-green-500 ap-rounded-full"></span>
