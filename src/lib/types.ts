705168ca5bc1334eb516daa9da4ebf6347beb2a3
n0lk <89748313+nzerolk@users.noreply.github.com>
2025-01-14 23:53:52 -0500

    Added support for ARC19 + bugfixes

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,18 @@
 export interface AssetMetadata {
     id: string
-    name: string
+    name: string,
+    amount: number,
+    decimals: number,
     description: string
     thumbnail: string
     type: 'arc72' | 'asa' | 'offchain'
+    subtype: 'arc3' | 'arc19' | undefined
     thumbnailMIMEType: 'image/png' | 'image/jpeg' | 'image/webp' | 'image/gif'
     properties: { [key: string]: string }
+}
+
+export interface Asset {
+    amount: number
+    "asset-id": number
+    "is-frozen": false
 }
