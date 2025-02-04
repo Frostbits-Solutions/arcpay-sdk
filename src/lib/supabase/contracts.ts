4799b7627d7b1324299581b4c1b88a5ee3df4297
n0lk <89748313+nzerolk@users.noreply.github.com>
2025-02-04 18:56:06 -0500

    Bumped contracts version

--- src/lib/supabase/contracts.ts
+++ src/lib/supabase/contracts.ts
@@ -4,7 +4,7 @@ import type {PublicNetwork} from "@/lib/algod/networks.config";
 export default async function getContract(supabase: SupabaseClient, network: PublicNetwork, contractTag: string): Promise<string> {
     const {data, error} = await supabase.from('contracts_tags_association').select(`
       contracts ( byte_code )
-    `).eq('chain', network).eq('tag', contractTag).eq('version', '0.0.9').returns<{ contracts: { byte_code: string } }[]>()
+    `).eq('chain', network).eq('tag', contractTag).eq('version', '0.0.10').returns<{ contracts: { byte_code: string } }[]>()
     if (error) throw new Error(`Error fetching contract: ${error.message}`)
     if (!data?.length) throw new Error(`Contract ${contractTag} not found`)
     return data[0].contracts.byte_code
